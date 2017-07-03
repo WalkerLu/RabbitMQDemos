1fab578c6b6b6e7e8ac629b4a2341558de230090
luwei.bj <luwei.bj>
2017-07-03 18:01:59 +0800

    init

---
+++ Receive/Receive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var conn = factory.CreateConnection())
+            {
+                using (var channel = conn.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Receive += (model, ea) =>
+                      {
+                          var body = ea.body;
+                          var message = Encoding.UTF8.GetString(body);
+                          System.Console.WriteLine(" [x] Receive {0}", message);
+
+                      };
+
+                    channel.BasicConsume(queue: "hello", noAck: true, consumer: consumer);
+                    System.Console.WriteLine(" press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+
+        }
+    }
+}
